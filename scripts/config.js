dc87385335d328475b82e6ad2d6db1e9a7a3b843
Andrey Polyakov <user2131@realweb.ru>
2022-05-04 21:42:30 +0300

    Добавлена логика работы с формой. Прописаны ф-ции на открытие и закрытие формы.

---
+++ scripts/config.js
@@ -0,0 +1,32 @@
+function openPlayerConfig(){
+    playerConfigOverlayElement.style.display = 'block';
+    backdropElement.style.display = 'block';
+
+}
+
+
+function closePlayerConfig(){
+    playerConfigOverlayElement.style.display = 'none';
+    backdropElement.style.display = 'none'; 
+    formElement.firstElementChild.classList.remove('error');
+    errorsOuputElement.textContent = '';
+
+}
+
+
+function savePlayerConfig(event){
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const enteredPlayerName = formData.get('playername').trim();
+    console.log(enteredPlayerName);
+    
+    if (!enteredPlayerName){
+        event.target.firstElementChild.classList.add('error');
+        errorsOuputElement.textContent = 'Вставьте корректное значение';
+        return;
+    }
+
+
+  
+
+}
